1ed4faff96494e35b02891646cb07264aafdf6c7
Austen Allred <austen.allred@gmail.com>
2014-01-11 11:50:47 -0700

    ex14

--- ex14.rb
+++ ex14.rb
@@ -1,1 +1,22 @@
-ex14.rb
+user = ARGV.first
+prompt = '> '
+
+puts "Hi, #{user}, I'm the #{$0} script"
+puts "I'd like to ask you a few questions."
+puts "Do you like me #{user}?"
+print prompt
+likes = STDIN.gets.chomp
+
+puts "Where are you from, #{user}?"
+print prompt
+lives = STDIN.gets.chomp()
+
+puts "What kindd of computer do you have, #{user}?"
+print prompt
+computer = STDIN.gets.chomp()
+
+puts <<MESSAGE
+Alright, so you said #{likes} about liking me.
+You're from #{lives}. Is that where kids dress up in their cowboy get-up to go to basketball games?
+And you have a #{computer}. Nice.
+MESSAGE
