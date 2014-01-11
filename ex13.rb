116e5766df84950b4aa28495c0c7a313f54814b1
Austen Allred <austen.allred@gmail.com>
2014-01-11 11:45:30 -0700

    modify ex13

--- ex13.rb
+++ ex13.rb
@@ -1,6 +1,6 @@
 var1, var2, var3 = ARGV
 
-puts "The script is called: #{0}"
+puts "The script is called: #{$0}"
 puts "your first variable is: #{var1}"
 puts "Your second variable is: #{var2}"
 puts "Your third variable is: #{var3}"
